4d7afa62a802598d96e16cf78af396383acc52d8
chensh <mail2chensh@gmail.com>
2015-05-25 01:52:07 -0700

    Initial Commit

---
+++ SKGDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SKGDemo
+//
+//  Created by chensh on 15-5-25.
+//  Copyright (c) 2015年 Chensh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SKGDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SKGDemo
+//
+//  Created by chensh on 15-5-25.
+//  Copyright (c) 2015年 Chensh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
